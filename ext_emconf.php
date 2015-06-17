c704111fd75b2e3a83113f41e9c44e2196df7df3
Loek Hilgersom <loek@netcoop.nl>
2015-06-17 22:49:16 +0200

    [TASK] Set version to 0.6.3-dev, dependencies: TYPO3 6.2.x and added retrostats dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'title' => 'ICS AWStats',
 	'description' => 'Includes the AWStats logfile analyzer as a backend module. This is a modified version of cc_awstats to support cron, reverse DNS lookups, ics_web_awstats and ics_beuser_awstats.',
 	'category' => 'module',
-	'version' => '0.6.2',
+	'version' => '0.6.3-dev',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -26,8 +26,9 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' => 
 		array (
-			'typo3' => '4.5.0-4.7.99',
+			'typo3' => '6.2.0-6.2.99',
 			'lang' => '',
+			'retrostats' => '',
 		),
 		'conflicts' => 
 		array (
